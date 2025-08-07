70baa48dafc5c40e300ba11f2f99652e4557877e
Hamza <ankouri.ha@gmail.com>
2025-08-07 23:16:15 +0100

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['host.docker.internal','ollama','agents', '0.0.0.0', '127.0.0.1', 'web.local', '172.22.145.216'], // 👈 ADD THIS LINE
+    allowedHosts: ['host.docker.internal','ollama','agents', '0.0.0.0', '127.0.0.1', 'web.local', '172.22.145.216'],
   },
   plugins: [react(), tailwindcss()],
   resolve: {
